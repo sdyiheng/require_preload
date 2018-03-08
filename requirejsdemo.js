c0c49b43ec380fb4d5b3b0e1c472083423eb9d93
sdyiheng <sdyiheng@gmail.com>
2018-03-08 14:54:54 +0800

    init commit

---
+++ requirejsdemo.js
@@ -0,0 +1,19 @@
+﻿
+//配置requirejs
+require.config({
+    baseUrl: '/requirejsdemo',
+    paths: {
+        'jquery': 'vendors/jquery-3.3.1.min',
+    },
+    shim: {
+    },
+    config: {
+    },
+    waitSeconds: 0,//,
+    //enforceDefine: true
+    preLoadedScripts: { "/requirejsdemo/url1.js":"define('url1',[],function(){alert('preloaded。');return {demo:1};})"}
+});
+require(["url1"],
+    function (u) {
+		alert(u.demo);
+    });
